b684e61e2312a747eeaec95647b44f9fb138b053
Miguel Sotolongo <miso@microsoft.com>
2019-01-26 14:30:18 -0800

    basic distortion

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,12 @@ import {
     Engine,
     Scene,
     FreeCamera,
-    HemisphericLight,
-    Vector3,
-    SceneLoader,
-    MeshBuilder,
-    Mesh
+    HemisphericLight, DirectionalLight,
+    Vector3, Color3,
+    SceneLoader
 } from "babylonjs";
 import 'babylonjs-loaders';
+import { Sea } from './sea';
 
 var canvas: any = document.getElementById("renderCanvas");
 var engine: Engine = new Engine(canvas, true);
@@ -16,12 +15,23 @@ var engine: Engine = new Engine(canvas, true);
 function createScene(): Scene {
     var scene: Scene = new Scene(engine);
 
-    var camera: FreeCamera = new FreeCamera("Camera", new Vector3(0, 0,-10), scene);
+    var camera: FreeCamera = new FreeCamera("Camera", new Vector3(0, 20, 0), scene);
     camera.attachControl(canvas, true);
 
-    var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+    var light: HemisphericLight = new HemisphericLight("light2", new BABYLON.Vector3(1, 1, 1), scene);
+	light.intensity = 0.6;
+	light.specular = Color3.Black();
+
+    var light2 = new DirectionalLight("dir01", new BABYLON.Vector3(0, -0.5, -1.0), scene);
+    light2.position = new Vector3(0, 5, 5);
+
     //var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1.0 }, scene);
-    SceneLoader.Append('/assets/3d/long_fish/', '12993_Long_Fin_White_Cloud_v1_l3.obj', scene, function (newMeshes) {});
+    SceneLoader.Append('/assets/3d/long_fish/', '12993_Long_Fin_White_Cloud_v1_l3.obj', scene, function (ctx:Scene) {
+        console.log(ctx);
+        camera.setTarget(ctx.meshes[0].position);
+        var sea : Sea = new Sea(scene);
+        sea.addToRenderList(ctx.meshes[0]);
+    });
     return scene;
 }
 
---
+++ src/sea.ts
@@ -0,0 +1,46 @@
+import {
+    Scene,
+    Color3,
+    Mesh, 
+    Texture,
+    Vector2,
+    CubeTexture
+} from "babylonjs";
+import { StandardMaterial, WaterMaterial } from 'babylonjs-materials';
+
+export class Sea {
+    private skybox: Mesh;
+    private skyboxMaterial: StandardMaterial;
+    private waterMesh : Mesh;
+    private water: WaterMaterial;
+
+    constructor(scene: Scene)
+    {
+        this.skybox = Mesh.CreateBox("skyBox", 5000.0, scene);
+        this.skyboxMaterial = new StandardMaterial("skyBox", scene);
+        this.skyboxMaterial.backFaceCulling = false;
+        this.skyboxMaterial.reflectionTexture = new CubeTexture("assets/textures/skybox/", scene);
+        this.skyboxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE;
+        this.skyboxMaterial.diffuseColor = new Color3(0, 0, 0);
+        this.skyboxMaterial.specularColor = new Color3(0, 0, 0);
+        this.skyboxMaterial.disableLighting = true;
+        this.skybox.material = this.skyboxMaterial;
+    
+        this.waterMesh = Mesh.CreateGround("waterMesh", 524, 524, 16, scene);
+        this.water = new WaterMaterial("water", scene);
+        this.water.backFaceCulling = true;
+        this.water.bumpTexture = new Texture("assets/textures/waterbump.png", scene);
+        this.water.windForce = -1;
+        this.water.waveHeight = 0.2;
+        this.water.bumpHeight = 0.02;
+        this.water.windDirection = new Vector2(1, 1);
+        this.water.waterColor = new Color3(0, 0, 221 / 255);
+        this.water.colorBlendFactor = 0.0;
+        this.water.addToRenderList(this.skybox);
+        this.waterMesh.material = this.water;
+    }
+
+    public addToRenderList(node: any) {
+        this.water.addToRenderList(node);
+    }
+}
