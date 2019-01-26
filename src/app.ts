b1bcd4085123e99b3525479f2dd2168403a261cc
Miguel Sotolongo <themorgwar@gmail.com>
2019-01-26 09:43:09 -0800

    webpack

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,22 @@
 import { Engine, Scene, FreeCamera, HemisphericLight, Vector3, MeshBuilder, Mesh } from "babylonjs";
 
 var canvas: any = document.getElementById("renderCanvas");
-var lastMouseEvent : MouseEvent;
-canvas.addEventListener('click', function(event: MouseEvent) { 
-    console.log(event);
-    lastMouseEvent = event;
-}, false);
-
 var engine: Engine = new Engine(canvas, true);
 
 function createScene(): Scene {
     var scene: Scene = new Scene(engine);
 
     var camera: FreeCamera = new FreeCamera("Camera", new Vector3(0, 0,-10), scene);
-    //camera.attachControl(canvas, true);
+    camera.attachControl(canvas, true);
 
     var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-
+    var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1.0 }, scene);
 
     return scene;
 }
 
 var scene: Scene = createScene();
-var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 0.25 }, scene);
 
 engine.runRenderLoop(() => {
     scene.render();
-    if(lastMouseEvent === undefined)
-        return;
-    console.log(sphere);
-    sphere.position.x = lastMouseEvent.x;
-    sphere.position.y = lastMouseEvent.y;
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
 const path = require("path");
 
 module.exports = {
-    entry: './index.ts',
+    entry: './src/app.ts',
     output: {
-        filename: 'index.js',
+        filename: 'app.js',
         path: path.resolve(__dirname, 'dist')
     },
     resolve: {
-        extensions: [".ts"]
+        extensions: [".ts", '.js']
     },
+    devtool: 'source-map',
+    mode: "development",
     module: {
-        rules: [
-            { test: /\.tsx?$/, loader: "ts-loader" }
-        ]
-    },
-    mode: "development"
+        rules: [{
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    }
 };
