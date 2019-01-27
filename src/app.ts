0f0a60a856b8f3ee7b45d00c6e38c4875bff8bde
Jacob Slone <jslone1993@LIVE.COM>
2019-01-26 16:31:35 -0800

    feat(Flocking): adding flocking behavior and cleaning up movement

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,36 @@
 import { Engine, Scene, FreeCamera, HemisphericLight, Vector3, MeshBuilder, Mesh } from "babylonjs";
 
-import PlayerFish from './game/fish';
+import { PlayerFishBehavior, FlockingFishBehavior, Flock } from './game/fish';
 
 var canvas: any = document.getElementById("renderCanvas");
 var engine: Engine = new Engine(canvas, true);
 
+function createFlockingFish(flock: Flock, index: number): FlockingFishBehavior {
+    let npcFishMesh = MeshBuilder.CreateSphere(`sphere_flocking_${index}`, { diameter: 1.0 }, scene);
+        npcFishMesh.position = new Vector3(Math.random(), 0, Math.random()).scale(5);
+
+        let flockingFish = new FlockingFishBehavior(npcFishMesh);
+        flockingFish.joinFlock(flock);
+
+        return flockingFish;
+}
+
 function createScene(): Scene {
     var scene: Scene = new Scene(engine);
 
-    var camera: FreeCamera = new FreeCamera("Camera", new Vector3(0, 10, 0), scene);
+    var camera: FreeCamera = new FreeCamera("Camera", new Vector3(0, 100, 0), scene);
 
     var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
     let ground = MeshBuilder.CreateGround("ground", { width: 100, height: 100 }, scene);
-    var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1.0 }, scene);
 
-    let fish = new PlayerFish(scene, ground, sphere);
+    var playerFishMesh: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1.0 }, scene);
+    let playerFish = new PlayerFishBehavior(scene, ground, playerFishMesh);
+
+    let flock = new Flock(playerFish);
+
+    for(let i = 0; i < 10; i++) {
+        createFlockingFish(flock, i);
+    }
 
     camera.setTarget(ground.position);
     return scene;
--- src/game/fish.ts
+++ src/game/fish.ts
@@ -1,18 +1,60 @@
-import { Mesh, Scene, Node, Nullable, Vector3, GroundGeometry } from "babylonjs";
+import { Mesh, Scene, Vector3, GroundGeometry } from "babylonjs";
 
 
-export default class PlayerFishBehavior {
+class PhysicsBehavior {
+    mesh: Mesh;
+
+    mass: number;
+    velocity: Vector3;
+    acceleration: Vector3;
+
+
+    constructor(mesh: Mesh, mass: number = 1.0) {
+        this.mesh = mesh;
+        this.mass = mass;
+
+        this.velocity = Vector3.Zero();
+        this.acceleration = Vector3.Zero();
+
+        this.mesh.onBeforeRenderObservable.add(this.update.bind(this));
+    }
+
+    applyForce(force: Vector3) {
+        this.acceleration = this.acceleration.add(force.scale(1.0 / this.mass));
+    }
+
+    updatePosition(deltaTime: number) {
+        this.mesh.rotate(Vector3.Up(), Math.atan2(this.velocity.x, this.velocity.z));
+
+        this.velocity.addInPlace(this.acceleration.scale(deltaTime));
+        this.mesh.position.addInPlace(this.velocity.scale(deltaTime));
+        this.mesh.position.y = 0;
+
+        this.acceleration = Vector3.Zero();
+    }
+
+    update() {
+        let deltaTime = 0.01;
+        this.updatePosition(deltaTime);
+    }
+}
+
+export class PlayerFishBehavior extends PhysicsBehavior {
     scene: Scene;
     ground: Mesh;
-    mesh: Mesh;
 
     target: Vector3;
     shouldFollowPointer: boolean;
-    speed: number;
+    chaseStrength: number;
 
-    constructor(scene: Scene, ground: Mesh, mesh: Mesh) {
+    constructor(scene: Scene,
+        ground: Mesh,
+        mesh: Mesh,
+        chaseStrength = 3.5) {
+        super(mesh);
+
         this.scene = scene;
-        this.mesh = mesh;
+        this.ground = ground;
 
         this.scene.onPointerObservable.add(pointerInfo => {
             switch (pointerInfo.type) {
@@ -31,14 +73,12 @@ export default class PlayerFishBehavior {
             }
         });
 
-        this.mesh.onBeforeRenderObservable.add(this.update.bind(this));
-
         this.shouldFollowPointer = false;
-        this.speed = 0.1;
+        this.chaseStrength = chaseStrength;
     }
 
     getGroundPoint() : Vector3 {
-        return this.scene.pick(this.scene.pointerX, this.scene.pointerY).pickedPoint;
+        return this.scene.pick(this.scene.pointerX, this.scene.pointerY, mesh => mesh == this.ground).pickedPoint;
     }
 
     update() : void {
@@ -47,11 +87,168 @@ export default class PlayerFishBehavior {
 
             let delta = target.subtract(this.mesh.position);
 
-            if(delta.length() > this.speed) {
-                delta = delta.normalize().scale(this.speed);
-            }
+            delta = delta.scale(this.chaseStrength);
 
-            this.mesh.position.addInPlace(delta);
+            this.velocity = delta;
         }
+
+        super.update();
+    }
+}
+
+export class Flock {
+    playerFish: PlayerFishBehavior;
+    flockingFish: FlockingFishBehavior[];
+
+    constructor(playerFish: PlayerFishBehavior) {
+        this.playerFish = playerFish;
+        this.flockingFish = [];
+    }
+
+    addFlockingFish(flockingFish: FlockingFishBehavior) {
+        this.flockingFish.push(flockingFish);
+    }
+
+    mergeFlocks(flock: Flock) {
+        let bigFlock: Flock;
+        let littleFlock: Flock;
+
+        if(this.flockingFish.length > flock.flockingFish.length) {
+            bigFlock = this;
+            littleFlock = flock;
+        }
+        else if(this.flockingFish.length < flock.flockingFish.length) {
+            bigFlock = flock;
+            littleFlock = this;
+        }
+        else if(Math.random() < 0.5) {
+            bigFlock = this;
+            littleFlock = flock;
+        }
+        else {
+            bigFlock = flock;
+            littleFlock = this;
+        }
+
+        littleFlock.flockingFish.forEach(fish => fish.joinFlock(bigFlock));
+    }
+}
+
+export class FlockingFishBehavior extends PhysicsBehavior {
+    flock: Flock;
+
+    separationDistance: number;
+    separationStrength: number;
+
+    cohesionDistance: number;
+    cohesionStrength: number;
+
+    alignmentDistance: number;
+    alignmentStrength: number;
+
+    constructor(mesh: Mesh,
+        mass: number = 1.0,
+        separationDistance: number = 5,
+        separationStrength: number = 5,
+        cohesionDistance: number = 20,
+        cohesionStrength: number = 10,
+        alignmentDistance: number = 20,
+        alignmentStrength: number = 0.1) {
+
+        super(mesh, mass);
+
+        this.separationDistance = separationDistance;
+        this.separationStrength = separationStrength;
+
+        this.cohesionDistance = cohesionDistance;
+        this.cohesionStrength = cohesionStrength;
+
+        this.alignmentDistance = alignmentDistance;
+        this.alignmentStrength = alignmentStrength;
+    }
+
+    joinFlock(flock: Flock) {
+        flock.addFlockingFish(this);
+        this.flock = flock;
+    }
+
+    applyForce(force: Vector3) {
+        this.acceleration = this.acceleration.add(force.scale(1.0 / this.mass));
+    }
+
+    update() {
+        let otherFish = this.flock.flockingFish
+            .filter(fish => fish != this);
+
+        let separationForce = this.calculateSeparation(otherFish, this.flock.playerFish);
+        this.applyForce(separationForce);
+
+        let cohesionForce = this.calculateCohesion(otherFish, this.flock.playerFish);
+        this.applyForce(cohesionForce);
+
+        let alignmentForce = this.calculateAlignment(otherFish, this.flock.playerFish);
+        this.applyForce(alignmentForce);
+
+        super.update();
+    }
+
+    calculateSeparation(flockingFish: FlockingFishBehavior[], playerFish: PlayerFishBehavior): Vector3 {
+        let separationForce = Vector3.Zero();
+
+        let calculateSeparation = (pos: Vector3) => {
+            let delta = this.mesh.position.subtract(pos);
+
+            if(delta.length() < this.separationDistance) {
+                let force = delta.normalize().scale(this.separationStrength / delta.length());
+                separationForce.addInPlace(force);
+            }
+        };
+
+        flockingFish.forEach(fish => calculateSeparation(fish.mesh.position));
+        calculateSeparation(playerFish.mesh.position);
+
+        return separationForce;
+    }
+
+    calculateCohesion(flockingFish: FlockingFishBehavior[], playerFish: PlayerFishBehavior): Vector3 {
+        let closeFlockMatePositionSum = Vector3.Zero();
+        let closeFlockMateCount = 0;
+
+        let calculateCohesion = (pos: Vector3) => {
+            let delta = this.mesh.position.subtract(pos);
+
+            if(delta.length() < this.cohesionDistance) {
+                closeFlockMatePositionSum.addInPlace(pos);
+                closeFlockMateCount++;
+            }
+        };
+
+        flockingFish.forEach(fish => calculateCohesion(fish.mesh.position));
+        calculateCohesion(playerFish.mesh.position);
+
+        let averagePosition = closeFlockMatePositionSum.scaleInPlace(1.0 / closeFlockMateCount);
+
+        return averagePosition.subtractInPlace(this.mesh.position).scaleInPlace(this.cohesionStrength);
+    }
+
+    calculateAlignment(flockingFish: FlockingFishBehavior[], playerFish: PlayerFishBehavior): Vector3 {
+        let averageVelocity = Vector3.Zero();
+        let closeFlockMateCount = 0;
+
+        let calculateAlignment = (pos: Vector3, velocity: Vector3) => {
+            let delta = this.mesh.position.subtract(pos);
+
+            if(delta.length() < this.alignmentDistance) {
+                averageVelocity.addInPlace(velocity);
+                closeFlockMateCount++;
+            }
+        }
+
+        flockingFish.forEach(fish => calculateAlignment(fish.mesh.position, fish.velocity));
+        calculateAlignment(playerFish.mesh.position, playerFish.velocity);
+
+        averageVelocity.scaleInPlace(1.0 / closeFlockMateCount);
+
+        return averageVelocity.scaleInPlace(this.alignmentStrength);
     }
 }
